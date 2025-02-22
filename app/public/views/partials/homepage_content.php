820a0a903464c22c618747c7e51607d8c450c7f0
Pasha <pavel.shulgat.ne@gmail.com>
2025-02-23 00:58:43 +0100

    Updated homepage assets and styles

--- app/public/views/partials/homepage_content.php
+++ app/public/views/partials/homepage_content.php
@@ -1,6 +1,56 @@
-
-<main class="container-fluid d-flex flex-column flex-grow-1 p-0">
-    <h1>Homepage</h1>
+<main class="container-fluid d-flex flex-column flex-grow-1 align-items-center p-0">
+    <section class="banner col-12 col-md-6 col-lg-5 col-xl-4">
+        <div>
+            <img src="/assets/images/homepage-banner.jpg" class="img-fluid" alt="Banner">
+        </div>
+    </section>
+    <section class="row">
+    <div class="col-12 col-md-6 col-lg-5 col-xl-4 ">
+        <div class="oval-container position-relative text-center">
+            <img src="/assets/images/oval2.png" class="img-fluid oval-image" alt="Oval">
+            <div class="text-overlay position-absolute top-50 start-50 translate-middle text-center w-100">
+                <h1 class="display-5 fw-bold mb-2">You really don't want to miss this!</h1>
+                <p class="lead w-100 w-md-75 mx-auto">Explore Haarlem like never before! Discover top events, music, food, and history during The Festival - four unforgettable days of excitement in the heart of the city!</p>
+            </div>
+        </div>
+    </div>
+    </section>
+    <section class="museum-section col-12 col-md-6 col-lg-5 col-xl-4 position-relative">
+        <div class="museum-banner position-relative">
+            <img src="/assets/images/museum-banner.png" class="img-fluid" alt="Museum">     
+        </div>
+        <div class="green-shape position-absolute left-0">
+            <img src="/assets/images/green-shape.png" class="img-fluid" alt="Green Shape">
+        </div>
+        <div class="yellow-shape position-absolute left-0">
+            <img src="/assets/images/yellow-shape.png" class="img-fluid" alt="Yellow Shape">
+        </div>
+    </section>
+    <section class="yummy-section col-12 col-md-6 col-lg-5 col-xl-4 position-relative">
+        <div class="yummy-banner position-relative">
+            <img src="/assets/images/yummy-banner.png" class="img-fluid" alt="Yummy">     
+        </div>
+        <div class="terracota-shape position-absolute top-0">
+            <img src="/assets/images/terracota-shape.png" class="img-fluid" alt="Terracota Shape">
+        </div>
+    </section>
+    <section class="strolls-section col-12 col-md-6 col-lg-5 col-xl-4 position-relative">
+        <div class="strolls-banner position-relative">
+            <img src="/assets/images/strolls-banner.png" class="img-fluid" alt="Strolls">     
+        </div>
+        <div class="punch-shape position-absolute top-0">
+            <img src="/assets/images/punch-shape.png" class="img-fluid" alt="Punch Shape">
+        </div>
+    </section>
+    <section class="dance-section col-12 col-md-6 col-lg-5 col-xl-4 position-relative">
+        <div class="dance-banner position-relative">
+            <img src="/assets/images/dance-banner.png" class="img-fluid" alt="Dance">     
+        </div>
+        <div class="blue-shape position-absolute">
+            <img src="/assets/images/blue-shape.png" class="img-fluid" alt="Blue Shape">
+        </div>
+    </section>
 </main>
+
 
 
