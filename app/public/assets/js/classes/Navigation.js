7c2b44af862bf56f6f24d116a0b3dfcde1db59e5
Anastasia Yurashchuk <asu45456@gmail.com>
2025-03-16 21:26:16 +0100

    Populate the sitemap from the database

--- app/public/assets/js/classes/Navigation.js
+++ app/public/assets/js/classes/Navigation.js
@@ -1,18 +1,30 @@
 export class Navigation {
     constructor() {
         this.navbar = document.getElementById('navbar');
+        this.sitemap = document.getElementById('sitemap');
 
         for (const { slug, menu_name } of events) {
             const navItem = this.createNavItem(slug, menu_name);
             this.navbar.appendChild(navItem);
+            const sitemapItem = this.createSitemapItem(slug, menu_name);
+            this.sitemap.appendChild(sitemapItem);
         }
     }
 
     createNavItem(slug, name) {
-        const listItem = document.createElement('li');
-        listItem.className = 'nav-item';
-        listItem.innerHTML = `<a class="nav-link" id="nav-item-${slug}" href="/${slug}">${name}</a>`;
+        const navItem = document.createElement('li');
+        navItem.className = 'nav-item';
+        navItem.innerHTML = `<a class="nav-link" id="nav-item-${slug}" href="/${slug}">${name}</a>`;
 
-        return listItem;
+        return navItem;
+    }
+
+    createSitemapItem(slug, name) {
+        const sitemapItem = document.createElement('a');
+        sitemapItem.className = 'footer-link';
+        sitemapItem.href = `/${slug}`;
+        sitemapItem.innerHTML = `${name}`;
+
+        return sitemapItem;
     }
 }
