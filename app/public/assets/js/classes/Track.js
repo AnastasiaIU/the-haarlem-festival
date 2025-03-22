76daa4ce0aced30ca61f7735a685f37c5b45c7ca
Anastasia Yurashchuk <asu45456@gmail.com>
2025-03-22 22:29:34 +0100

    Add Spotify tracks

---
+++ app/public/assets/js/classes/Track.js
@@ -0,0 +1,57 @@
+/**
+ * Class that handles the tracks.
+ */
+export class Track {
+    async init() {
+        this.pathSegments = window.location.pathname.split('/').filter(segment => segment !== '');
+        this.slug = this.pathSegments[this.pathSegments.length - 1];
+
+        await this.populateTracks();
+    }
+
+    static async create() {
+        const instance = new Track();
+        await instance.init();
+        return instance;
+    }
+
+    /**
+     * Populates the artist tracks section with Spotify embedded players based on the artist slug.
+     */
+    async populateTracks() {
+        const artistTracks = document.getElementById('artistTracks');
+
+        const tracks = {
+            'hardwell': ['6L5xbckRDXIf5K1pwTaGkD', '49ZuJYjop8st6JwHWquK0u', '0GOXlShsWOp7BQ7nt9uwUL', '3EQ70Nq9D03VLRblD2JlJZ'],
+            'armin-van-buuren': ['5GjnIpUlLGEIYk052ISOw9', '1moFkZDqcjQNeXtyoanLHv', '6wLqNGHQIja6xqT0cfrzBB', '2Fv1x10CiHukDdu96CYeHc'],
+            'martin-garrix': ['23L5CiUhw2jV1OIMwthR3S', '3ebXMykcMXOcLeJ9xZ17XH', '52dEZA0A4siRTuA4e8w3ll', '7KPcippmg9MvPzb3dzNpQW'],
+            'tiesto': ['09CnYHiZ5jGT1wr1TXJ9Zt', '36gcliMRX1vCpgnrZE3dFZ', '3IhM5Mber8KA0NaRNpK2px', '3j11iDncb7ZeDMw7lFucqM'],
+            'nicky-romero': ['1sh6lL6cmlcwhqZKGiKBua', '4utBk1Fp8UwMbc1YBcoA4D', '1eL80CN0UF5aBsncWBPfFQ', '2bsyecmZCgdlsCZ3sWVZ99'],
+            'afrojack': ['4QNpBfC0zvjKqPJcyqBy9W', '5Ok52kEHxudhX1n2RvqyIe', '5VtRjcZYvXj1VdT3FWG3ZD', '5DdDbJvoaT8fqQMJkiGg4T']
+        }
+
+        if (tracks[this.slug]) {
+            for (const trackId of tracks[this.slug]) {
+                const track = this.createTrack(trackId);
+                artistTracks.appendChild(track);
+            }
+        }
+    }
+
+    /**
+     * Creates and returns a Spotify embed iframe for a given track ID.
+     *
+     * @param {string} trackId - The Spotify track ID.
+     * @returns {HTMLIFrameElement} The iframe element to be appended to the DOM.
+     */
+    createTrack(trackId) {
+        const iframe = document.createElement('iframe');
+        iframe.src = `https://open.spotify.com/embed/track/${trackId}`;
+        iframe.width = '300';
+        iframe.height = '80';
+        iframe.allowTransparency = true;
+        iframe.allow = 'encrypted-media';
+
+        return iframe;
+    }
+}
--- app/views/partials/events/dance/artist.php
+++ app/views/partials/events/dance/artist.php
@@ -1,7 +1,8 @@
 <section id="artistMainContent" class="d-flex flex-column align-items-center p-3 p-lg-5 gap-5">
     <div class="hero-shape-fix" id="heroShapeFix"></div>
-    <section id="artistTracks" class="col-lg-10 col-xl-8 d-flex flex-column gap-5 m-0 p-0">
-        <?php include __DIR__ . '/track.php' ?>
+    <section class="d-flex flex-column">
+        <h2 class="tracks-header h4 fw-bold">Tracks</h2>
+        <div id="artistTracks" class="d-flex flex-wrap gap-5 m-0 p-0"></div>
     </section>
     <section id="artistCarousel" class="d-flex flex-column w-100 gap-5 m-0 p-0">
         <?php include __DIR__ . '/../../carousel_one.php' ?>
