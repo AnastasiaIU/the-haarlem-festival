c0003a11e3dabce8c886c4d6506ced552fe43d8c
Pasha <pavel.shulgat.ne@gmail.com>
2025-03-31 19:59:45 +0200

    Commit to do rebase

--- app/routes/profile.php
+++ app/routes/profile.php
@@ -8,14 +8,14 @@ Route::add('/profile', function () {
     require_once(__DIR__ . "/../views/pages/profile.php");
 });
 
-Route::add('/profile/edit', function () {
-    AuthHandler::checkUserLoggedIn();
-
-    require_once(__DIR__ . "/../views/pages/profile_edit_info.php");
-});
 
 Route::add('/profile/personal-plan', function () {
     AuthHandler::checkUserLoggedIn();
 
     require_once(__DIR__ . "/../views/pages/profile_personal_plan.php");
+});
+Route::add('/profile/manage_events', function (){
+    AuthHandler::checkAdminLoggedIn();
+
+    require_once(__DIR__ . "/../views/pages/manage_events.php");
 });
---
+++ app/views/partials/manage_events.php
@@ -0,0 +1,3 @@
+<main class="container-fluid d-flex flex-column flex-grow-1 align-items-center p-0">
+    
+</main>
